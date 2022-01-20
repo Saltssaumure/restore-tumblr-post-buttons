690a3920d626ab76956b5827788cbdb2bf67a410
Saltssaumure <saltssaumure@gmail.com>
2022-01-20 22:52:08 +0000

    Create RestoreTumblrPostButtons.user.js

---
+++ RestoreTumblrPostButtons.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         RestoreTumblrPostButtons
+// @namespace    https://github.com/Saltssaumure/restore-tumblr-post-buttons
+// @version      0.1
+// @description  Restores the reblog/like/reply buttons to their original positions.
+// @author       Saltssaumure
+// @grant        GM_addStyle
+// @run-at       document-start
+// @include      https://www.tumblr.com/dashboard*
+// ==/UserScript==
+
+(function() {
+
+    GM_addStyle (`
+
+    /* Footer box */
+    .G6mnk .MCavR {
+        justify-content: flex-end;
+    }
+
+    /* Reblog etc. buttons */
+    .G6mnk .MCavR .sfGru {
+        margin-left: 1.5rem;
+    }
+
+    /* Note count */
+    .gstmW {
+        position: absolute;
+        bottom: 10px;
+    }
+
+    `)
+
+}) ();
